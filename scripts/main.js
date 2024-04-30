079dd333aeff21c2dd7d10351f4ff320b0e2e9eb
linux649 <ethan.su.649@gmail.com>
2024-05-01 08:40:39 +1200

    Update HTML and JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-
+func generate_task() {
+	var ppl = ["Alex","Zunairah","Celeste","Sofia","Charlotte","Ethan Lister","Ethan Su","Livia","Chloe","Nathan","Evelyn","Anyane","Kevin","Jamie","Simon","Taj","Harvey","JP","Noah","Anna","Melody"]
+	var tasks = ["Say hello to ","Start a conversation with "]
+	
+	
+}
