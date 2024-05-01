6deaa932bb192f85b66869797f9eaa5583fccfb8
linux649 <104065079+linux649@users.noreply.github.com>
2024-05-01 21:22:10 +1200

    Added debug alert for troubleshooting

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 func generate_task() {
+	alert("Function activated")
 	var ppl = ["Alex","Zunairah","Celeste","Sofia","Charlotte","Ethan Lister","Ethan Su","Livia","Chloe","Nathan","Evelyn","Anyane","Kevin","Jamie","Simon","Taj","Harvey","JP","Noah","Anna","Melody"]
 	var tasks = ["Say hello to ","Start a conversation with "]
 	var randomppl = array[Math.floor(Math.random() * ppl.length)];
